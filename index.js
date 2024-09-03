ec56481a5b38e067c03fd80dca8397c7a6a0417f
aswazone <aswamanu143@gmail.com>
2024-09-04 00:04:01 +0530

    send() for json

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 const PORT = 3000;
 
 app.get('/',(req,res)=>{
-
+    res.send('hello home page');
+});
+app.get('/about',(req,res)=>{
+    // res.writeHead(200,{'Content-Type':'text/html'});
+    res.send({message: "hello about page"});
 });
 
 app.listen(PORT,()=>{
